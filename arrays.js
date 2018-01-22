0a41d77263382b82e3cbde90228309e8cb40a992
Hang Pham <tpham.hang@gmail.com>
2018-01-22 00:58:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,6 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
