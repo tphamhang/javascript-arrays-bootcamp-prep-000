f83870d95c19e786e5000f9ff3ba1aa25382af63
Hang Pham <tpham.hang@gmail.com>
2018-01-22 00:48:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
-addElementToBeginningOfArray(array, element) = ["MM", ...chocolateBars]
 chocolateBars = addElementToBeginningOfArray(array, element)
+addElementToBeginningOfArray(array, element) = ["MM", ...chocolateBars]
