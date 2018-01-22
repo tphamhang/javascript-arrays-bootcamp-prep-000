ea3801682252ad991cd29e651f728b5b911d9581
Hang Pham <tpham.hang@gmail.com>
2018-01-22 01:24:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,6 +26,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array
+  return array.slice(1)
 }
