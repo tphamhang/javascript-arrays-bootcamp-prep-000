66e4daeb8e21e21012a1165f62f0182eb3248eec
Hang Pham <tpham.hang@gmail.com>
2018-01-22 00:03:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+addElementToBeginningOfArray = [chocolateBars...,"mast"]
+
+addElementToBeginningOfArray()
+
+destructivelyAddElementToBeginningOfArray = chocolateBars.unshift("mast")
+
+destructivelyAddElementToBeginningOfArray()
